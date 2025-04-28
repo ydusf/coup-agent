251f19365f5fc659a99f8b47e488a04e919e22b2
Sebastian Latham <sebastian.tlatham@gmail.com>
2025-04-28 22:16:52 +0100

    game loop improved

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ class Game:
                                              Captain(), Captain(), Captain(),
                                              Ambassador(), Ambassador(), Ambassador()]
         self._players: List[Player] = []
-        self._current_player: Optional[Player] = None
+        self._current_player_idx: Optional[int] = None
         self._game_active: bool = False
         self._winner: Optional[Player] = None
 
@@ -24,8 +24,12 @@ class Game:
         if len(self._players) < 2:
             raise ValueError("game cannot start with less than 2 players")
         
+        if len(self._players) > 7:
+            raise ValueError("game cannot start with more than 7 players as there are only 15 cards and each player needs two")
+        
         self._game_active = True
         self.__deal_coins__()
+        self.__deal_characters__()
         self.__choose_starting_player__()
 
         # game loop
@@ -33,17 +37,31 @@ class Game:
             if len(self._players) == 1:
                 self.__declare_winner__()
                 return
-                        
-            # game gets interesting here because we need to account for lies  
+            
+            # game gets interesting here because we need to account for lies
+            current_player: Player = self._players[self._current_player_idx]
+            play: str = current_player.ask_for_input()
+
+
+
+
+            self.__goto_next_player__()
 
     def __choose_starting_player__(self) -> None:
         random.shuffle(self._players)
-        starting_player_idx = random.randrange(len(self._players))
-        self._current_player = self._players[starting_player_idx]
+        self._current_player_idx = random.randrange(len(self._players))
+
+    def __goto_next_player__(self) -> Player:
+        self._current_player_idx = (self._current_player_idx + 1) % len(self._players)
 
     def __deal_coins__(self) -> None:
         for player in self._players:
             player.coins = 2
+
+    def __deal_characters__(self) -> None:
+        for player in self._players:
+            player.add_character(self._characters.pop())
+            player.add_character(self._characters.pop())
 
     def __declare_winner__(self):
         self._game_active = False
--- player.py
+++ player.py
@@ -1,10 +1,12 @@
+from character import Character
+
 from typing import List
 
 class Player:
     def __init__(self, name: str):
         self._coins: int = 0
         self._name: str = name
-        self._characters: List
+        self._characters: List[Character] = []
 
     @property
     def name(self) -> str:
@@ -21,8 +23,24 @@ class Player:
         
         self._coins = value
 
-    def take_income(self) -> None:
+    def ask_for_input(self) -> str:
+        play = input(
+            f"{self.name}'s move:\n"
+            "  [i] Take income (+1 coin)\n"
+            "  [f] Take foreign aid (+2 coins)\n"
+            "  [m] Use Duke's active (+3 coins)\n"
+            "  [s] Use Captain's active (steal 2 coins)\n"
+            "  [e] Use Ambassador's active (exchange characters)\n"
+            "  [a] Use Assassin's active (-3 coins to assassinate)\n"
+            "Your choice: "
+        )
+        return play
+
+    def add_character(self, character: Character) -> None:
+        self._characters.append(character)
+
+    def __take_income__(self) -> None:
         self._coins += 1
 
-    def take_foreign_aid(self) -> None:
+    def __take_foreign_aid__(self) -> None:
         self._coins += 2
